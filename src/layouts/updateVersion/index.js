e309d9cff58e25fbebfaf9fac1f28ee6b53bde55
duc2899 <ducne020899@gmail.com>
2026-04-17 01:11:58 +0700

    Update set version client

--- src/layouts/updateVersion/index.js
+++ src/layouts/updateVersion/index.js
@@ -10,10 +10,15 @@ import { useAlert } from "context/AlertContext";
 import adminAPI from "api/adminAPI";
 import MDDialog from "components/MDDialog";
 import MDInput from "components/MDInput";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 const UpdateVersion = ({ open, handleClose }) => {
   const { showAlert } = useAlert();
-  const [oldVersion, setOldVersion] = useState("");
+  const [data, setData] = useState([]);
+  const [oldVersion, setOldVersion] = useState({
+    type: "",
+    version: ""
+  })
 
   const {
     register,
@@ -23,13 +28,15 @@ const UpdateVersion = ({ open, handleClose }) => {
   } = useForm({
     defaultValues: {
       version: "",
+      type: ""
     },
   });
 
   const fetchData = async () => {
     try {
       const result = await adminAPI.getVersionClient();
-      setOldVersion(result.data.version);
+
+      setData(result.data);
 
       // reset input version mới về rỗng khi mở dialog
       reset({ version: "" });
@@ -40,18 +47,42 @@ const UpdateVersion = ({ open, handleClose }) => {
 
   useEffect(() => {
     if (open) fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [open]);
 
   const onSubmit = async (data) => {
     try {
-      await adminAPI.setVersionClient({ version: data.version });
-      showAlert("Đã gửi yêu cầu cập nhật phiên bản", "success");
+      if (!oldVersion.type) {
+        showAlert("Vui lòng chọn Loại", "warning");
+        return
+      }
+      await adminAPI.setVersionClient({ version: data.version, type: oldVersion.type });
+      showAlert("Cập nhật phiên bản thành công", "success");
       handleClose();
     } catch (error) {
       showAlert(error?.errors?.[0]?.message || "Update version thất bại", "error");
     }
   };
+
+  const handelChangeViewVersion = (e) => {
+    const value = e.target.value
+    setOldVersion({
+      type: value._id,
+      version: value.data.version
+    })
+  }
+
+  const getLangVersion = (key) => {
+    switch (key) {
+      case "CLIENT_VERSION_DEV":
+        return "DEV"
+      case "CLIENT_VERSION":
+        return "LIVE"
+      default:
+        return "Not found Type"
+    }
+  }
+
 
   return (
     <MDDialog
@@ -60,8 +91,29 @@ const UpdateVersion = ({ open, handleClose }) => {
       title="Thay đổi phiên bản"
       content={
         <MDBox>
+          <FormControl
+            sx={{
+              width: "100%",
+              marginBottom: "30px"
+            }}
+          >
+            <InputLabel>Loại</InputLabel>
+
+            <Select
+              // value={filter.type}
+              label="Loại"
+              onChange={handelChangeViewVersion}
+              sx={{ pt: 1.5, pb: 1.5 }}
+            >
+              {data.map((s) => (
+                <MenuItem key={s._id} value={s}>
+                  {getLangVersion(s._id)}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <MDInput
-            value={oldVersion || "Not found version"}
+            value={oldVersion.version || "Not found version"}
             label="Phiên bản hiện tại"
             fullWidth
             disabled
